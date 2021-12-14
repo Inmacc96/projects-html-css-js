7ca2d28f35f53ee9d17fbf6a94f1550f2575f5df
inmacc96 <inmacaballerocarrero@gmail.com>
2021-12-14 18:42:33 +0100

    Adding operations to the calculator application.

--- calculator-app/js/app.js
+++ calculator-app/js/app.js
@@ -8,3 +8,37 @@ function sum() {
   }
   document.getElementById("result").innerHTML = `Result: ${result}`;
 }
+
+function subtraction(){
+    const form = document.getElementById("forma");
+    let operandA = form["operandA"];
+    let operandB = form["operandB"];
+    let result = parseInt(operandA.value) - parseInt(operandB.value);
+  if (isNaN(result)) {
+    result = "The operation does not include numbers.";
+  }
+  document.getElementById("result").innerHTML = `Result: ${result}`;
+
+}
+
+function multiply(){
+    const form = document.getElementById("forma");
+    let operandA = form["operandA"];
+    let operandB = form["operandB"];
+    let result = parseInt(operandA.value) * parseInt(operandB.value);
+  if (isNaN(result)) {
+    result = "The operation does not include numbers.";
+  }
+  document.getElementById("result").innerHTML = `Result: ${result}`;
+}
+
+function divide(){
+    const form = document.getElementById("forma");
+    let operandA = form["operandA"];
+    let operandB = form["operandB"];
+    let result = parseInt(operandA.value) / parseInt(operandB.value);
+  if (isNaN(result)) {
+    result = "The operation does not include numbers.";
+  }
+  document.getElementById("result").innerHTML = `Result: ${result}`;
+}
