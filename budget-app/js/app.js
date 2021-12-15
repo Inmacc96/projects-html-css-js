f5ba65891365b5841ed29f767ae3974b19fbe727
inmacc96 <inmacaballerocarrero@gmail.com>
2021-12-15 21:04:22 +0100

    Define the functions totalIncome, totalExpenditure to calculate total income and total expenditure.

--- budget-app/js/app.js
+++ budget-app/js/app.js
@@ -5,3 +5,24 @@ const expenditures = [
   new Expenditure("Clothing", 400),
 ];
 
+let loadApp = () => {
+  loadHeader();
+};
+
+let totalIncome = () => {
+  let totalIncome = 0;
+  for (let income of revenues) {
+    totalIncome += income.value;
+  }
+  return totalIncome;
+};
+
+let totalExpenditures = () => {
+    let totalExpenditures = 0;
+    for(let expenditure of expenditures){
+        totalExpenditures += expenditure.value;
+    }
+    return totalExpenditures;
+};
+
+
