bec54586ae363094523441c9cdd25fe3a9e2ecf7
inmacc96 <inmacaballerocarrero@gmail.com>
2021-12-15 21:02:59 +0100

    Define the revenues and expenditures arrays.

---
+++ budget-app/js/app.js
@@ -0,0 +1,7 @@
+const revenues = [new Income("Salary", 2100.0), new Income("Sale car", 1500)];
+
+const expenditures = [
+  new Expenditure("Rent flat", 900),
+  new Expenditure("Clothing", 400),
+];
+
