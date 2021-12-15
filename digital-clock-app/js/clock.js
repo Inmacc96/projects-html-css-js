a3174fbe89a8b62bf783096622a6431dbc67b273
inmacc96 <inmacaballerocarrero@gmail.com>
2021-12-15 17:56:21 +0100

    Adding the date and defining a format for the time.

--- digital-clock-app/js/clock.js
+++ digital-clock-app/js/clock.js
@@ -1,9 +1,29 @@
 const showClock = () => {
     let date = new Date();
-    let hour = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
+    let hour = formatTime(date.getHours());
+    let min = formatTime(date.getMinutes());
+    let sec = formatTime(date.getSeconds());
     document.getElementById('time').innerHTML = `${hour}:${min}:${sec}`;
+
+    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dic'];
+    const days = ['Sun','Mon','Tues','Wed','Thurs','Fri','Sat'];
+    let dayWeek = days[date.getDay()];
+    let day = date.getDate();
+    let month = months[date.getMonth()];
+    let year = date.getFullYear();
+    let dateText = `${dayWeek}, ${day} ${month} ${year}`;
+    document.getElementById('date').innerHTML = dateText;
+    
+
+    
+}
+
+const formatTime = (time) => {
+    if( time < 10){
+        time = '0' + time;
+    }
+    return time;
 }
 
 setInterval(showClock, 1000);
+
