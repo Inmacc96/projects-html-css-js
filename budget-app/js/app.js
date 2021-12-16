a0fea66c1c6c995735c7125563b3dbf497f7614c
inmacc96 <inmacaballerocarrero@gmail.com>
2021-12-16 12:53:14 +0100

    Creating the functions loadRevenues and createIncomeHTML to dynamically generate the income list items.

--- budget-app/js/app.js
+++ budget-app/js/app.js
@@ -1,4 +1,4 @@
-const revenues = [new Income("Salary", 2100.0), new Income("Sale car", 1500)];
+const revenues = [new Income("Salary", 2100.00), new Income("Sale car", 1500)];
 
 const expenditures = [
   new Expenditure("Rent flat", 900),
@@ -7,6 +7,7 @@ const expenditures = [
 
 let loadApp = () => {
   loadHeader();
+  loadRevenues();
 };
 
 let totalIncome = () => {
@@ -18,27 +19,61 @@ let totalIncome = () => {
 };
 
 let totalExpenditures = () => {
-    let totalExpenditures = 0;
-    for(let expenditure of expenditures){
-        totalExpenditures += expenditure.value;
-    }
-    return totalExpenditures;
+  let totalExpenditures = 0;
+  for (let expenditure of expenditures) {
+    totalExpenditures += expenditure.value;
+  }
+  return totalExpenditures;
 };
 
 let loadHeader = () => {
-    let budget = totalIncome() - totalExpenditures();
-    let percentage = totalExpenditures()/totalIncome();
-    document.getElementById('budget_total').innerHTML = formatCurrency(budget);
-    document.getElementById('percentage').innerHTML = formatPercentage(percentage);
-    document.getElementById('income').innerHTML = formatCurrency(totalIncome());
-    document.getElementById('expenditure').innerHTML = formatCurrency(totalExpenditures());
-  };
+  let budget = totalIncome() - totalExpenditures();
+  let percentage = totalExpenditures() / totalIncome();
+  document.getElementById("budget_total").innerHTML = formatCurrency(budget);
+  document.getElementById("percentage").innerHTML =
+    formatPercentage(percentage);
+  document.getElementById("income").innerHTML = formatCurrency(totalIncome());
+  document.getElementById("expenditure").innerHTML = formatCurrency(
+    totalExpenditures()
+  );
+};
 
-  const formatCurrency = (value) => {
-    return value.toLocaleString('en-US',{style:'currency', currency:'USD', minimumFractionDigits: 2});
+const formatCurrency = (value) => {
+  return value.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  });
+};
+
+const formatPercentage = (value) => {
+  return value.toLocaleString("en-US", {
+    style: "percent",
+    minimumFractionDigits: 2,
+  });
+};
+
+const loadRevenues = () => {
+  let revenuesHTML = "";
+  for (let income of revenues) {
+    revenuesHTML += createIncomeHTML(income);
   }
+  document.getElementById("list-revenues").innerHTML = revenuesHTML;
+};
 
-  const formatPercentage = (value) => {
-    return value.toLocaleString('en-US',{style:'percent', minimumFractionDigits:2})
-  }
-
+const createIncomeHTML = (income) => {
+  let incomeHTML = `
+    <div class="element cleanStyles">
+        <div class="element_description">${income.description}</div>
+          <div class="right cleanStyles">
+            <div class="element_value">${formatCurrency(income.value)}</div>
+            <div class="element_remove">
+                <button class="element_remove--btn">
+                  <ion-icon name="close-circle-outline"></ion-icon>
+                </button>
+            </div>
+        </div>
+      </div>
+    `;
+  return incomeHTML;
+};
